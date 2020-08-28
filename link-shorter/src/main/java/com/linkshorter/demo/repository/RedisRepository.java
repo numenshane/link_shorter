3a9474670dc676cc8a1cd3f10e16f4fdcb6dc0dd
池田 <numenshane@gmail.com>
2020-08-28 11:52:03 +0800

    Update RedisRepository.java

--- link-shorter/src/main/java/com/linkshorter/demo/repository/RedisRepository.java
+++ link-shorter/src/main/java/com/linkshorter/demo/repository/RedisRepository.java
@@ -26,8 +26,6 @@ public class RedisRepository {
         jedisPoolConfig.setMinIdle(5);
         // timeout，这里既是连接超时又是读写超时，从Jedis 2.8开始有区分connectionTimeout和soTimeout的构造函数
         jedisPool = new JedisPool(jedisPoolConfig, DemoApplication.getArgsByName("redis_ip"), Integer.valueOf(DemoApplication.getArgsByName("redis_port")), 3000, null);
-//        jedisPool = new JedisPool(jedisPoolConfig, "13.212.9.160", 6379, 3000, null);
-
     }
 
     public void setString(String key, String value) {
@@ -48,13 +46,11 @@ public class RedisRepository {
     public String getString(String key) {
         Jedis jedis = null;
         try {
-            //从redis连接池里拿出一个连接执行命令
             jedis = jedisPool.getResource();
             return jedis.get(key);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            //注意这里不是关闭连接，在JedisPool模式下，Jedis会被归还给资源池。
             if (jedis != null)
                 jedis.close();
         }
